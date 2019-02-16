f5f8ec4a8d1af792412049c4575bfb76ce31bd7c
YuriDuque <yurithielmann83@gmail.com>
2019-02-16 01:40:02 -0200

    removendo projetos de teste

--- WebAPi/WebApi_EntityFramework/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebApi_EntityFramework.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- WebAPi/WebApi_EntityFramework/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApi_EntityFramework.WebApiApplication" Language="C#" %>
